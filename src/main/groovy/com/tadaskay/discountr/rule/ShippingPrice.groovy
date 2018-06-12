d81ffbf2c0bc70bae71a460f297d15c6e5447827
Tadas Krivickas <tadas.krivickas@gmail.com>
2018-06-13 00:50:42 +0300

    Format price/discount as per specification

--- src/main/groovy/com/tadaskay/discountr/rule/ShippingPrice.groovy
+++ src/main/groovy/com/tadaskay/discountr/rule/ShippingPrice.groovy
@@ -1,10 +1,19 @@
 package com.tadaskay.discountr.rule
 
+import java.text.DecimalFormat
+
 class ShippingPrice {
     BigDecimal price
     BigDecimal discount
 
     String toString() {
-        [price, discount != 0 ? discount : '-'].join(' ')
+        [
+            decimalFormat(price),
+            discount ? decimalFormat(discount) : '-'
+        ].join(' ')
+    }
+
+    static String decimalFormat(decimal) {
+        new DecimalFormat('0.00').format(decimal)
     }
 }
